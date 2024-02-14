475b809710958b61830acf14af5c0adae81e9d33
unknown <shuvalov.rem@mail.ru>
2024-02-14 10:39:51 +0800

    added tagtypes, worked with apislice

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,5 +1,8 @@
 import {
-    useGetTodosQuery
+    useGetTodosQuery,
+    useAddTodoMutation,
+    useUpdateTodoMutation,
+    useDeleteTodoMutation,
 } from '../api/apiSlice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
@@ -15,10 +18,13 @@ const TodoList = () => {
         isError,
         error
     } = useGetTodosQuery();
+    const [addTodo] = useAddTodoMutation();
+    const [updateTodo] = useUpdateTodoMutation();
+    const [deleteTodo] = useDeleteTodoMutation();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        //addTodo
+        addTodo({ userId: 1, title: newTodo, completed: false })
         setNewTodo('')
     }
 
@@ -44,7 +50,24 @@ const TodoList = () => {
     if (isLoading) {
         content = <p>Loading...</p>
     } else if (isSuccess) {
-        content = JSON.stringify(todos)
+        content = todos.map(todo => {
+            return (
+                <article key={todo.id}>
+                    <div className="todo">
+                        <input
+                            type="checkbox"
+                            checked={todo.completed}
+                            id={todo.id}
+                            onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
+                        />
+                        <label htmlFor={todo.id}>{todo.title}</label>
+                    </div>
+                    <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                </article>
+            )
+        })
     } else if (isError) {
         content = <p>{error}</p>
     }
